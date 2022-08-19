e86bf5ff7ea74a2c11ca7351cc19f566837aef71
Burak <burak.yurtoglu.eem@gmail.com>
2022-08-19 13:19:18 +0300

    Add project files.

---
+++ RazorPizzaApp/Pages/Orders/Orders.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzaApp.Data;
+using RazorPizzaApp.Models;
+
+namespace RazorPizzaApp.Pages
+{
+    public class OrdersModel : PageModel
+    {
+        public List<PizzaOrder> PizzaOrders { get; set; }
+
+        private readonly ApplicationDbContext _context;
+
+        public OrdersModel(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public void OnGet()
+        {
+            PizzaOrders = _context.PizzaOrders.ToList();
+
+
+
+        }
+    }
+}
